7316a62a70a6e591117caecefad958a92f4d1c0a
SAPP Academy <sapp.academy2025@gmail.com>
2025-11-23 10:17:22 +0300

    Removed unused brevo import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
-import Brevo from "@getbrevo/brevo"; // <-- NEW: Brevo SDK
 
 const app = express();
 app.use(cors());
